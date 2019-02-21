27d82ae213cf3451eeab99cf73354523ebc54820
Ling Jiang <jiangling91@outlook.com>
2019-02-21 13:39:34 +0800

    Add files via upload

---
+++ Week2-Programming Assignment 1/corr.R
@@ -0,0 +1,58 @@
+corr <- function(directory, threshold = 0){
+    
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'threshold' is a numeric vector of length 1 indicating the
+    ## number of completely observed observations (on all
+    ## variables) required to compute the correlation between
+    ## nitrate and sulfate; the default is 0
+    
+    ## Return a numeric vector of correlations
+    ## NOTE: Do not round the result!
+    
+    # set working directory
+    setwd("~/文档/Week2")
+    #calculate nobs for each file using complete function
+    complete_table <- complete(directory)
+    nobs <- complete_table$nobs
+    #get ids of those files that have the number of nobs greater than threshold
+    ids <- complete_table$id[nobs > threshold]
+    
+    # get a list of files in specdata
+    file <- list.files(directory,full.names = TRUE)
+    result <- vector(mode = "numeric", length=0)
+    n <- 1
+    for (i in ids){
+        current_file <- read.csv(file[i])
+        result[n] <- cor(current_file$sulfate,current_file$nitrate,use="complete.obs")
+        n<-n+1
+    }
+    return(result)
+}
+
+# Quiz answers
+# Question 8
+# What value is printed at end of the following code?
+cr <- corr("specdata")                
+cr <- sort(cr)                
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+# Question 9
+# What value is printed at end of the following code?
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)                
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+# Question 10
+# What value is printed at end of the following code?
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
